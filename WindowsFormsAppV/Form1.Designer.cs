5ef88041f9262b414f1b781587b8ea70f00c9df5
Usuario <Usuario@CPD02-M03>
2024-08-20 12:28:15 -0300

    Adicionar arquivos de projeto.

---
+++ WindowsFormsAppV/Form1.Designer.cs
@@ -0,0 +1,366 @@
+﻿namespace WindowsFormsAppV
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.lblSUL = new System.Windows.Forms.Label();
+            this.lblSUDESTE = new System.Windows.Forms.Label();
+            this.lblCENTRO = new System.Windows.Forms.Label();
+            this.lblNORDESTE = new System.Windows.Forms.Label();
+            this.lblNORTE = new System.Windows.Forms.Label();
+            this.lblTODOS = new System.Windows.Forms.Label();
+            this.cmbTODOS = new System.Windows.Forms.ComboBox();
+            this.cmbNORTE = new System.Windows.Forms.ComboBox();
+            this.cmbCENTRO = new System.Windows.Forms.ComboBox();
+            this.cmbNORDESTE = new System.Windows.Forms.ComboBox();
+            this.cmbSUL = new System.Windows.Forms.ComboBox();
+            this.cmbSUDESTE = new System.Windows.Forms.ComboBox();
+            this.btnTODOS = new System.Windows.Forms.Button();
+            this.btnNORTE = new System.Windows.Forms.Button();
+            this.btnNORDESTE = new System.Windows.Forms.Button();
+            this.btnCENTRO = new System.Windows.Forms.Button();
+            this.btnSUDESTE = new System.Windows.Forms.Button();
+            this.btnSUL = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(63, 69);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(63, 127);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "label2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(63, 178);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "label3";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(63, 237);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(35, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "label4";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(63, 310);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(35, 13);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "label5";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(63, 386);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(35, 13);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "label6";
+            // 
+            // lblSUL
+            // 
+            this.lblSUL.AutoSize = true;
+            this.lblSUL.Location = new System.Drawing.Point(675, 386);
+            this.lblSUL.Name = "lblSUL";
+            this.lblSUL.Size = new System.Drawing.Size(13, 13);
+            this.lblSUL.TabIndex = 11;
+            this.lblSUL.Text = "0";
+            // 
+            // lblSUDESTE
+            // 
+            this.lblSUDESTE.AutoSize = true;
+            this.lblSUDESTE.Location = new System.Drawing.Point(675, 310);
+            this.lblSUDESTE.Name = "lblSUDESTE";
+            this.lblSUDESTE.Size = new System.Drawing.Size(13, 13);
+            this.lblSUDESTE.TabIndex = 10;
+            this.lblSUDESTE.Text = "0";
+            // 
+            // lblCENTRO
+            // 
+            this.lblCENTRO.AutoSize = true;
+            this.lblCENTRO.Location = new System.Drawing.Point(675, 237);
+            this.lblCENTRO.Name = "lblCENTRO";
+            this.lblCENTRO.Size = new System.Drawing.Size(13, 13);
+            this.lblCENTRO.TabIndex = 9;
+            this.lblCENTRO.Text = "0";
+            // 
+            // lblNORDESTE
+            // 
+            this.lblNORDESTE.AutoSize = true;
+            this.lblNORDESTE.Location = new System.Drawing.Point(675, 178);
+            this.lblNORDESTE.Name = "lblNORDESTE";
+            this.lblNORDESTE.Size = new System.Drawing.Size(13, 13);
+            this.lblNORDESTE.TabIndex = 8;
+            this.lblNORDESTE.Text = "0";
+            // 
+            // lblNORTE
+            // 
+            this.lblNORTE.AutoSize = true;
+            this.lblNORTE.Location = new System.Drawing.Point(675, 127);
+            this.lblNORTE.Name = "lblNORTE";
+            this.lblNORTE.Size = new System.Drawing.Size(13, 13);
+            this.lblNORTE.TabIndex = 7;
+            this.lblNORTE.Text = "0";
+            // 
+            // lblTODOS
+            // 
+            this.lblTODOS.AutoSize = true;
+            this.lblTODOS.Location = new System.Drawing.Point(675, 69);
+            this.lblTODOS.Name = "lblTODOS";
+            this.lblTODOS.Size = new System.Drawing.Size(19, 13);
+            this.lblTODOS.TabIndex = 6;
+            this.lblTODOS.Text = "27";
+            // 
+            // cmbTODOS
+            // 
+            this.cmbTODOS.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbTODOS.FormattingEnabled = true;
+            this.cmbTODOS.Items.AddRange(new object[] {
+            "AC",
+            "AL",
+            "AP",
+            "AM",
+            "BA",
+            "CE",
+            "DF",
+            "ES",
+            "GO",
+            "MA",
+            "MT",
+            "MS",
+            "MG",
+            "PA",
+            "PB",
+            "PR",
+            "PE",
+            "PI",
+            "RS",
+            "RJ",
+            "RO",
+            "RN",
+            "RR",
+            "SC",
+            "SP",
+            "SE",
+            "TO"});
+            this.cmbTODOS.Location = new System.Drawing.Point(162, 61);
+            this.cmbTODOS.Name = "cmbTODOS";
+            this.cmbTODOS.Size = new System.Drawing.Size(269, 21);
+            this.cmbTODOS.TabIndex = 12;
+            // 
+            // cmbNORTE
+            // 
+            this.cmbNORTE.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbNORTE.FormattingEnabled = true;
+            this.cmbNORTE.Location = new System.Drawing.Point(162, 119);
+            this.cmbNORTE.Name = "cmbNORTE";
+            this.cmbNORTE.Size = new System.Drawing.Size(269, 21);
+            this.cmbNORTE.TabIndex = 13;
+            // 
+            // cmbCENTRO
+            // 
+            this.cmbCENTRO.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbCENTRO.FormattingEnabled = true;
+            this.cmbCENTRO.Location = new System.Drawing.Point(165, 244);
+            this.cmbCENTRO.Name = "cmbCENTRO";
+            this.cmbCENTRO.Size = new System.Drawing.Size(269, 21);
+            this.cmbCENTRO.TabIndex = 15;
+            // 
+            // cmbNORDESTE
+            // 
+            this.cmbNORDESTE.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbNORDESTE.FormattingEnabled = true;
+            this.cmbNORDESTE.Location = new System.Drawing.Point(165, 186);
+            this.cmbNORDESTE.Name = "cmbNORDESTE";
+            this.cmbNORDESTE.Size = new System.Drawing.Size(269, 21);
+            this.cmbNORDESTE.TabIndex = 14;
+            // 
+            // cmbSUL
+            // 
+            this.cmbSUL.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbSUL.FormattingEnabled = true;
+            this.cmbSUL.Location = new System.Drawing.Point(162, 383);
+            this.cmbSUL.Name = "cmbSUL";
+            this.cmbSUL.Size = new System.Drawing.Size(269, 21);
+            this.cmbSUL.TabIndex = 17;
+            // 
+            // cmbSUDESTE
+            // 
+            this.cmbSUDESTE.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbSUDESTE.FormattingEnabled = true;
+            this.cmbSUDESTE.Location = new System.Drawing.Point(162, 325);
+            this.cmbSUDESTE.Name = "cmbSUDESTE";
+            this.cmbSUDESTE.Size = new System.Drawing.Size(269, 21);
+            this.cmbSUDESTE.TabIndex = 16;
+            // 
+            // btnTODOS
+            // 
+            this.btnTODOS.Location = new System.Drawing.Point(558, 59);
+            this.btnTODOS.Name = "btnTODOS";
+            this.btnTODOS.Size = new System.Drawing.Size(75, 23);
+            this.btnTODOS.TabIndex = 18;
+            this.btnTODOS.Text = "-";
+            this.btnTODOS.UseVisualStyleBackColor = true;
+            // 
+            // btnNORTE
+            // 
+            this.btnNORTE.Location = new System.Drawing.Point(558, 117);
+            this.btnNORTE.Name = "btnNORTE";
+            this.btnNORTE.Size = new System.Drawing.Size(75, 23);
+            this.btnNORTE.TabIndex = 19;
+            this.btnNORTE.Text = "-";
+            this.btnNORTE.UseVisualStyleBackColor = true;
+            this.btnNORTE.Click += new System.EventHandler(this.btnNORTE_Click);
+            // 
+            // btnNORDESTE
+            // 
+            this.btnNORDESTE.Location = new System.Drawing.Point(558, 168);
+            this.btnNORDESTE.Name = "btnNORDESTE";
+            this.btnNORDESTE.Size = new System.Drawing.Size(75, 23);
+            this.btnNORDESTE.TabIndex = 20;
+            this.btnNORDESTE.Text = "-";
+            this.btnNORDESTE.UseVisualStyleBackColor = true;
+            // 
+            // btnCENTRO
+            // 
+            this.btnCENTRO.Location = new System.Drawing.Point(558, 232);
+            this.btnCENTRO.Name = "btnCENTRO";
+            this.btnCENTRO.Size = new System.Drawing.Size(75, 23);
+            this.btnCENTRO.TabIndex = 21;
+            this.btnCENTRO.Text = "-";
+            this.btnCENTRO.UseVisualStyleBackColor = true;
+            // 
+            // btnSUDESTE
+            // 
+            this.btnSUDESTE.Location = new System.Drawing.Point(558, 300);
+            this.btnSUDESTE.Name = "btnSUDESTE";
+            this.btnSUDESTE.Size = new System.Drawing.Size(75, 23);
+            this.btnSUDESTE.TabIndex = 22;
+            this.btnSUDESTE.Text = "-";
+            this.btnSUDESTE.UseVisualStyleBackColor = true;
+            // 
+            // btnSUL
+            // 
+            this.btnSUL.Location = new System.Drawing.Point(558, 386);
+            this.btnSUL.Name = "btnSUL";
+            this.btnSUL.Size = new System.Drawing.Size(75, 23);
+            this.btnSUL.TabIndex = 23;
+            this.btnSUL.Text = "-";
+            this.btnSUL.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnSUL);
+            this.Controls.Add(this.btnSUDESTE);
+            this.Controls.Add(this.btnCENTRO);
+            this.Controls.Add(this.btnNORDESTE);
+            this.Controls.Add(this.btnNORTE);
+            this.Controls.Add(this.btnTODOS);
+            this.Controls.Add(this.cmbSUL);
+            this.Controls.Add(this.cmbSUDESTE);
+            this.Controls.Add(this.cmbCENTRO);
+            this.Controls.Add(this.cmbNORDESTE);
+            this.Controls.Add(this.cmbNORTE);
+            this.Controls.Add(this.cmbTODOS);
+            this.Controls.Add(this.lblSUL);
+            this.Controls.Add(this.lblSUDESTE);
+            this.Controls.Add(this.lblCENTRO);
+            this.Controls.Add(this.lblNORDESTE);
+            this.Controls.Add(this.lblNORTE);
+            this.Controls.Add(this.lblTODOS);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label lblSUL;
+        private System.Windows.Forms.Label lblSUDESTE;
+        private System.Windows.Forms.Label lblCENTRO;
+        private System.Windows.Forms.Label lblNORDESTE;
+        private System.Windows.Forms.Label lblNORTE;
+        private System.Windows.Forms.Label lblTODOS;
+        private System.Windows.Forms.ComboBox cmbTODOS;
+        private System.Windows.Forms.ComboBox cmbNORTE;
+        private System.Windows.Forms.ComboBox cmbCENTRO;
+        private System.Windows.Forms.ComboBox cmbNORDESTE;
+        private System.Windows.Forms.ComboBox cmbSUL;
+        private System.Windows.Forms.ComboBox cmbSUDESTE;
+        private System.Windows.Forms.Button btnTODOS;
+        private System.Windows.Forms.Button btnNORTE;
+        private System.Windows.Forms.Button btnNORDESTE;
+        private System.Windows.Forms.Button btnCENTRO;
+        private System.Windows.Forms.Button btnSUDESTE;
+        private System.Windows.Forms.Button btnSUL;
+    }
+}
+
---
+++ WindowsFormsAppV/Form1.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsAppV
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnNORTE_Click(object sender, EventArgs e)
+        {
+            if (cmbNORTE.SelectedIndex != -1)
+            {
+                cmbTODOS.Items.Add(cmbNORTE.SelectedItem);
+
+                cmbNORTE.Items.RemoveAt(cmbNORTE.SelectedIndex);
+
+                lblNORTE.Text = cmbNORTE.Items.Count.ToString();
+
+                lblTODOS.Text = cmbTODOS.Items.Count.ToString();
+            }
+        }
+
+        private void btnNORDESTE_Click(object sender, EventArgs e)
+        {
+            if (cmbNORTE.SelectedIndex != -1)
+            {
+                cmbTODOS.Items.Add(cmbNORDESTE.SelectedItem);
+
+                cmbNORDESTE.Items.RemoveAt(cmbNORDESTE.SelectedIndex);
+
+                lblNORDESTE.Text = cmbNORDESTE.Items.Count.ToString();
+
+                lblTODOS.Text = cmbTODOS.Items.Count.ToString();
+            }
+        }
+
+        private void btnCENTRO_Click(object sender, EventArgs e)
+        {
+            if (cmbNORTE.SelectedIndex != -1)
+            {
+                cmbTODOS.Items.Add(cmbCENTRO.SelectedItem);
+
+                cmbCENTRO.Items.RemoveAt(cmbCENTRO.SelectedIndex);
+
+                lblCENTRO.Text = cmbCENTRO.Items.Count.ToString();
+
+                lblTODOS.Text = cmbTODOS.Items.Count.ToString();
+            }
+        }
+
+        private void btnSUDESTE_Click(object sender, EventArgs e)
+        {
+            if (cmbNORTE.SelectedIndex != -1)
+            {
+                cmbTODOS.Items.Add(cmbSUDESTE.SelectedItem);
+
+                cmbSUDESTE.Items.RemoveAt(cmbSUDESTE.SelectedIndex);
+
+                lblSUDESTE.Text = cmbSUDESTE.Items.Count.ToString();
+
+                lblTODOS.Text = cmbTODOS.Items.Count.ToString();
+            }
+        }
+
+        private void btnSUL_Click(object sender, EventArgs e)
+        {
+            if (cmbNORTE.SelectedIndex != -1)
+            {
+                cmbTODOS.Items.Add(cmbSUL.SelectedItem);
+
+                cmbSUL.Items.RemoveAt(cmbSUL.SelectedIndex);
+
+                lblSUL.Text = cmbSUL.Items.Count.ToString();
+
+                lblTODOS.Text = cmbTODOS.Items.Count.ToString();
+            }
+        }
+    }
+}
